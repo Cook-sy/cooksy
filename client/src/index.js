d8ce175afc350b524ab8ca1bdf850c5f248f4d75
John Cheng <chengj@gmail.com>
2017-05-26 16:47:13 -0700

    Change client folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
