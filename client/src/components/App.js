f4794449bc5ca386b33cd667207a73ca9f3217ab
John Cheng <chengj@gmail.com>
2017-05-26 16:58:52 -0700

    Add server proxy for client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,20 @@ import logo from '../images/logo.svg';
 import '../css/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(res => this.setState({users: res.results}));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +27,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <ul>
+          { this.state.users.map((user, i) => {
+              return (
+                <li key={i}>{user.name}</li>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
