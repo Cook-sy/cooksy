2e000e600a069be08bb57dcb7eebc0608ae8186c
John Cheng <chengj@gmail.com>
2017-05-26 14:24:17 -0700

    Add server logging

--- server-config.js
+++ server-config.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var indexRoutes = require('./src/routes/index');
 
 var app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('common'));
+} else {
+  app.use(morgan('dev'));
+}
 
 app.use('/', indexRoutes);
 
