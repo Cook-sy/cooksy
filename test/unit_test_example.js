03f8c19ed4f272f6f5e2646efbbe70af8d98bfde
obay <obaymardini@gmail.com>
2017-05-27 23:12:56 +0200

    Add an example of how to do a basic unit test using sinon-test and other dependencies

---
+++ test/unit_test_example.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var sinon = require('sinon');
+
+var expect = chai.expect;
+var sinonTest = require('sinon-test');
+
+// use sinon.test to automatically restore your stubs
+sinon.test = sinonTest.configureTest(sinon);
+sinon.testCase = sinonTest.configureTestCase(sinon);
+
+describe('Name of the file you are testing', function() {
+  it('some test case', sinon.test(function() {
+  }));
+  
+});
