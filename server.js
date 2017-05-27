d377fd38afe1fe2328a10bdf7969308059d0f792
John Cheng <chengj@gmail.com>
2017-05-26 21:02:31 -0700

    Disable eslint for specific line

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = require('./src/server-config');
 var port = process.env.PORT || 3001;
 
 app.listen(port, function() {
-  /* eslint-disable no-console */
+  // eslint-disable-next-line no-console
   console.log('Find the server at: http://localhost:' + port);
-  /* eslint-enable no-console */
 });
