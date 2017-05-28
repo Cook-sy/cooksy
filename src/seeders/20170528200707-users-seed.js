5ad30ecae7b22b825ea5e12e264b98d70cdcb63b
John Cheng <chengj@gmail.com>
2017-05-28 15:05:20 -0700

    Change user seed to use faker

--- src/seeders/20170528200707-users-seed.js
+++ src/seeders/20170528200707-users-seed.js
@@ -1,58 +1,23 @@
 'use strict';
 
+var faker = require('faker');
+faker.seed(38448);
+
+var users = [];
+
+for (var i = 0; i < 5; i++) {
+  users.push({
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    zipcode: faker.address.zipCode(),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent()
+  });
+}
+
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-    return queryInterface.bulkInsert(
-      'Users',
-      [
-        {
-          username: 'bono',
-          password: 'hunter2',
-          zipcode: '12345',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          username: 'jono',
-          password: 'hunter2',
-          zipcode: '00123',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          username: 'rono',
-          password: 'hunter2',
-          zipcode: '54213',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          username: 'vono',
-          password: 'hunter2',
-          zipcode: '66120',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          username: 'oono',
-          password: 'hunter2',
-          zipcode: '99912',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ],
-      {}
-    );
+    return queryInterface.bulkInsert('Users', users, {});
   },
 
   down: function(queryInterface, Sequelize) {
