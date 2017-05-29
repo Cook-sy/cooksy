9a6cf427c80cde26b6ccbd96ed1396ee90a823d2
vicki alden <vic.alden23@gmail.com>
2017-05-29 16:28:38 -0700

    Add strategy for logging in users

---
+++ src/passport/local-login-user.js
@@ -0,0 +1,53 @@
+var jwt = require('jsonwebtoken');
+var Strategy = require('passport-local').Strategy;
+var db = require('../models');
+
+module.exports = new Strategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password',
+    session: false,
+    passReqToCallback: true
+  },
+  function(req, username, password, done) {
+    var userData = {
+      username: username.trim(),
+      password: password.trim()
+    };
+
+    return db.User
+      .findOne({
+        where: {
+          username: userData.username
+        }
+      })
+      .then(function(user) {
+        if (!user) {
+          var error = new Error('Incorrect username or password');
+          error.name = 'IncorrectCredentialsError';
+
+          return done(error);
+        }
+
+        return user.comparePassword(userData.password).then(function(isMatch) {
+          if (!isMatch) {
+            var error = new Error('Incorrect username or password');
+            error.name = 'IncorrectCredentialsError';
+
+            return done(error);
+          }
+
+          var payload = {
+            sub: user.id
+          };
+
+          var token = jwt.sign(payload, process.env.JWT_SECRET);
+
+          return done(null, token);
+        });
+      })
+      .catch(function(err) {
+        return done(err);
+      });
+  }
+);
