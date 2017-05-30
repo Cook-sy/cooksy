c12ab8c15be8c3b499fde7d96daf062dadd86cd5
John Cheng <chengj@gmail.com>
2017-05-30 15:12:39 -0700

    Add auth check for chef

---
+++ src/middleware/is-authenticated.js
@@ -0,0 +1,53 @@
+var jwt = require('jsonwebtoken');
+var db = require('../models');
+
+exports.isChef = function(req, res, next) {
+  if (!req.header('x-access-token')) {
+    return res.status(401).send({
+      success: false,
+      message: 'Please login or sign up'
+    });
+  }
+
+  var token = req.header('x-access-token').split(' ')[1];
+
+  return jwt.verify(token, process.env.JWT_SECRET, function(err, decodedToken) {
+    if (err) {
+      return res.status(401).send({
+        success: false,
+        message: 'Please login or sign up'
+      });
+    }
+
+    var userId = decodedToken.sub;
+    var role = decodedToken.role;
+
+    if (role !== 'chef') {
+      return res.status(403).send({
+        success: false,
+        message: 'You are forbidden'
+      });
+    }
+
+    return db.Chef
+      .findById(userId)
+      .then(function(user) {
+        if (!user) {
+          return res.status(401).send({
+            success: false,
+            message: 'Please sign up'
+          });
+        }
+
+        return next();
+      })
+      .catch(function() {
+        return res.status(500).send({
+          success: false,
+          message: 'Please try again later'
+        });
+      });
+  });
+};
+
+exports.isUser = function(req, res, next) {};
