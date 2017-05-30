0d8ea8a296b1d8479add7cb8821c81ef414732c0
Robert Si <robertsi0214@gmail.com>
2017-05-30 15:27:14 -0700

    create Meal List and Meal Detail

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import axios from 'axios';
 
 export const CREATE_MEAL = 'CREATE_MEAL';
@@ -8,5 +9,13 @@ export function createMeal(values, cb) {
   return {
     type: CREATE_MEAL,
     payload: Request
+=======
+export function selectMeal(meal) {
+  //selectmeal is an action creator, an obj with a type property
+  //action ALWAYS contains a type, sometimes a payload
+  return {
+    type: 'MEAL_SELECTED',
+    payload: meal
+>>>>>>> create Meal List and Meal Detail
   };
 }
