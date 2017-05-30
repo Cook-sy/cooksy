d2186f08a135f3afcbb6830689560768c7dc3818
obay <obaymardini@gmail.com>
2017-05-30 23:22:10 +0200

    (feat) Add material-ui theme to tha app and Add redux-promise middleware to it

--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Promise from 'redux-promise';
 
 import reducers from './reducers';
 import App from './components/App';
-import NewPostForm from './containers/NewPostForm';
+import NewMealForm from './containers/NewMealForm';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
     <BrowserRouter>
       <div>
       	<Switch>
           <Route exact path="/" component={App} />
-          <Route exact path="/post-new-meal" component={NewPostForm} />
+          <Route exact path="/post-new-meal" component={NewMealForm} />
       	</Switch>
       </div>
     </BrowserRouter>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
