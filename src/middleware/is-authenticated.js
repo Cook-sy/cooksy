4ba5a7ce6577d7f69fadc37438b1e5412050d5ed
John Cheng <chengj@gmail.com>
2017-05-30 15:39:58 -0700

    Refactor auth check middleware

--- src/middleware/is-authenticated.js
+++ src/middleware/is-authenticated.js
@@ -1,100 +1,60 @@
 var jwt = require('jsonwebtoken');
 var db = require('../models');
 
-exports.isChef = function(req, res, next) {
-  if (!req.header('x-access-token')) {
-    return res.status(401).send({
-      success: false,
-      message: 'Please login or sign up'
-    });
-  }
-
-  var token = req.header('x-access-token').split(' ')[1];
-
-  return jwt.verify(token, process.env.JWT_SECRET, function(err, decodedToken) {
-    if (err) {
+function isAuthenticated(model, role) {
+  return function(req, res, next) {
+    if (!req.header('x-access-token')) {
       return res.status(401).send({
         success: false,
         message: 'Please login or sign up'
       });
     }
 
-    var userId = decodedToken.sub;
-    var role = decodedToken.role;
+    var token = req.header('x-access-token').split(' ')[1];
 
-    if (role !== 'chef') {
-      return res.status(403).send({
-        success: false,
-        message: 'You are forbidden'
-      });
-    }
-
-    return db.Chef
-      .findById(userId)
-      .then(function(user) {
-        if (!user) {
-          return res.status(401).send({
-            success: false,
-            message: 'Please sign up'
-          });
-        }
-
-        return next();
-      })
-      .catch(function() {
-        return res.status(500).send({
+    return jwt.verify(token, process.env.JWT_SECRET, function(
+      err,
+      decodedToken
+    ) {
+      if (err) {
+        return res.status(401).send({
           success: false,
-          message: 'Please try again later'
+          message: 'Please login or sign up'
         });
-      });
-  });
-};
+      }
 
-exports.isUser = function(req, res, next) {
-  if (!req.header('x-access-token')) {
-    return res.status(401).send({
-      success: false,
-      message: 'Please login or sign up'
+      var userId = decodedToken.sub;
+      var userRole = decodedToken.role;
+
+      if (userRole !== role) {
+        return res.status(403).send({
+          success: false,
+          message: 'You are forbidden'
+        });
+      }
+
+      return model
+        .findById(userId)
+        .then(function(user) {
+          if (!user) {
+            return res.status(401).send({
+              success: false,
+              message: 'Please sign up'
+            });
+          }
+
+          return next();
+        })
+        .catch(function() {
+          return res.status(500).send({
+            success: false,
+            message: 'Please try again later'
+          });
+        });
     });
-  }
+  };
+}
 
-  var token = req.header('x-access-token').split(' ')[1];
+exports.isChef = isAuthenticated(db.Chef, 'chef');
 
-  return jwt.verify(token, process.env.JWT_SECRET, function(err, decodedToken) {
-    if (err) {
-      return res.status(401).send({
-        success: false,
-        message: 'Please login or sign up'
-      });
-    }
-
-    var userId = decodedToken.sub;
-    var role = decodedToken.role;
-
-    if (role !== 'user') {
-      return res.status(403).send({
-        success: false,
-        message: 'You are forbidden'
-      });
-    }
-
-    return db.User
-      .findById(userId)
-      .then(function(user) {
-        if (!user) {
-          return res.status(401).send({
-            success: false,
-            message: 'Please sign up'
-          });
-        }
-
-        return next();
-      })
-      .catch(function() {
-        return res.status(500).send({
-          success: false,
-          message: 'Please try again later'
-        });
-      });
-  });
-};
+exports.isUser = isAuthenticated(db.User, 'user');
