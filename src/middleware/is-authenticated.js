f2a78c2433a8df57f003fe4bb2fa4475e455403e
John Cheng <chengj@gmail.com>
2017-05-30 15:32:11 -0700

    Add auth check for user

--- src/middleware/is-authenticated.js
+++ src/middleware/is-authenticated.js
@@ -50,4 +50,51 @@ exports.isChef = function(req, res, next) {
   });
 };
 
-exports.isUser = function(req, res, next) {};
+exports.isUser = function(req, res, next) {
+  if (!req.header('x-access-token')) {
+    return res.status(401).send({
+      success: false,
+      message: 'Please login or sign up'
+    });
+  }
+
+  var token = req.header('x-access-token').split(' ')[1];
+
+  return jwt.verify(token, process.env.JWT_SECRET, function(err, decodedToken) {
+    if (err) {
+      return res.status(401).send({
+        success: false,
+        message: 'Please login or sign up'
+      });
+    }
+
+    var userId = decodedToken.sub;
+    var role = decodedToken.role;
+
+    if (role !== 'user') {
+      return res.status(403).send({
+        success: false,
+        message: 'You are forbidden'
+      });
+    }
+
+    return db.User
+      .findById(userId)
+      .then(function(user) {
+        if (!user) {
+          return res.status(401).send({
+            success: false,
+            message: 'Please sign up'
+          });
+        }
+
+        return next();
+      })
+      .catch(function() {
+        return res.status(500).send({
+          success: false,
+          message: 'Please try again later'
+        });
+      });
+  });
+};
