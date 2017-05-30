29e66f6bda8b58f676bd86f913a3f516710ce0d8
obay <obaymardini@gmail.com>
2017-05-31 01:47:41 +0200

    (feat) Mocked the generated Id

--- client/src/containers/NewMealForm.test.js
+++ client/src/containers/NewMealForm.test.js
@@ -9,10 +9,18 @@ import { Provider } from 'react-redux';
 
 import { NewMealForm } from './NewMealForm';
 
+// mock the randomly generated element's id
+jest.mock('uuid', () => {
+  return {
+    v4: jest.fn(() => 'testID')
+  };
+});
+
 const store = createStore(() => ({}));
 const Decorated = reduxForm({ form: 'NewMealForm' })(NewMealForm);
 
 describe('NewMealForm', () => {
+
   it('renders without crashing', () => {
     shallow(<Decorated />);
   });
