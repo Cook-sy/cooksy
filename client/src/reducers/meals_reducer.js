65c6fbde1edbd67ff2ddf72f74f51ab4716aa009
obay <obaymardini@gmail.com>
2017-05-31 02:56:13 +0200

    (feat) Fix indentations in meals_reducer file

--- client/src/reducers/meals_reducer.js
+++ client/src/reducers/meals_reducer.js
@@ -4,9 +4,9 @@ let initialState = {};
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-  	case (CREATE_MEAL) :
-  	  return state;
-  	default :
-  		return state;
+    case CREATE_MEAL:
+      return state;
+    default:
+      return state;
   }
 }
