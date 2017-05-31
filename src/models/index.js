ae88ef7f42a8e4d62bf045fbd0e09d526fdb6d60
John Cheng <chengj@gmail.com>
2017-05-30 22:36:31 -0700

    (test) Disable sequelize logging when testing

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ var db = {};
 
 var sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
