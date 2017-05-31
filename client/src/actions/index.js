b33f81bb46430c4f896663cbbe18ce022deefdeb
obay <obaymardini@gmail.com>
2017-05-31 02:53:37 +0200

    (feat) Fix the url and get rid of ROOT_URL

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,14 +2,11 @@ import axios from 'axios';
 
 export const CREATE_MEAL = 'CREATE_MEAL';
 
-const ROOT_URL = 'http://localhost:3001';
 export function createMeal(values, cb) {
-  const request = axios.post(`${ROOT_URL}/api/chefs/meals`, values)
-    .then(cb)
+  const request = axios.post('/api/chefs/meals', values).then(cb);
 
   return {
     type: CREATE_MEAL,
     payload: Request
-  }
-  
+  };
 }
