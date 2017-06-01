cf866e11aa322c565fc5017ac353f89bba2b647b
obay <obaymardini@gmail.com>
2017-06-01 05:43:27 +0200

    (feat) Create a new reducer to update authentication data

---
+++ client/src/reducers/authentication_reducer.js
@@ -0,0 +1,27 @@
+import { SIGN_UP_USER, SIGN_UP_CHEF } from '../actions';
+
+let initialState = {
+  auth: {
+  	isLoggedIn: false,
+  	role: false
+  }
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SIGN_UP_USER :
+      return { auth: {
+			  	isLoggedIn: true,
+			  	role: 'user'
+		  	} 
+			};
+    case SIGN_UP_CHEF :
+      return { auth: {
+			  	isLoggedIn: true,
+			  	role: 'chef'
+		  	} 
+			};
+    default:
+      return state;
+  }
+}
