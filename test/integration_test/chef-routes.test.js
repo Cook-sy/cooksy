b0cdd04024dac708a7eeb7f30065c6cbfc57b2d9
John Cheng <chengj@gmail.com>
2017-06-01 16:33:53 -0700

    (cleanup) Move some test hooks

--- test/integration_test/chef-routes.test.js
+++ test/integration_test/chef-routes.test.js
@@ -9,77 +9,77 @@ chai.use(require('chai-json-schema'));
 var app = require('../../src/server-config');
 
 describe('/api/chefs', function() {
+  var chefToken;
+  var userToken;
+
+  var mealObj = {
+    name: 'rubber',
+    description: 'tastee',
+    deliveryDateTime: '2018-04-02T00:57:18Z',
+    pickupInfo: 'knock please',
+    price: '5.12',
+    servings: 20,
+    images: '',
+    address: '123 Main Ave',
+    city: 'Metro',
+    state: 'AI',
+    zipcode: '29921'
+  };
+
+  before(function(done) {
+    var chefSignup = request(app)
+    .post('/api/chefs/signup')
+    .send({
+      username: 'oicki',
+      password: 'hunter2',
+      image: '',
+      address: '123 Main St.',
+      city: 'Springfield',
+      state: 'IL',
+      zipcode: '00000'
+    })
+    .then(function(res) {
+      chefToken = res.body.token;
+    });
+
+    var userSignup = request(app)
+    .post('/api/users/signup')
+    .send({
+      username: 'oicki',
+      password: 'hunter2',
+      zipcode: '00000'
+    })
+    .then(function(res) {
+      userToken = res.body.token;
+    });
+
+    Promise.all([chefSignup, userSignup]).then(function() {
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    db.Meal.destroy({
+      where: { name: 'rubber' }
+    }).then(function() {
+      done();
+    });
+  });
+
+  after(function(done) {
+    var chefRemove = db.Chef.destroy({
+      where: { username: 'oicki' }
+    });
+    var userRemove = db.User.destroy({
+      where: { username: 'oicki' }
+    });
+
+    Promise.all([chefRemove, userRemove]).then(function() {
+      done();
+    });
+  });
+
   describe('Create a new meal', function() {
-    var chefToken;
-    var userToken;
-
-    var mealObj = {
-      name: 'rubber',
-      description: 'tastee',
-      deliveryDateTime: '2018-04-02T00:57:18Z',
-      pickupInfo: 'knock please',
-      price: '5.12',
-      servings: 20,
-      images: '',
-      address: '123 Main Ave',
-      city: 'Metro',
-      state: 'AI',
-      zipcode: '29921'
-    };
-
-    before(function(done) {
-      var chefSignup = request(app)
-        .post('/api/chefs/signup')
-        .send({
-          username: 'oicki',
-          password: 'hunter2',
-          image: '',
-          address: '123 Main St.',
-          city: 'Springfield',
-          state: 'IL',
-          zipcode: '00000'
-        })
-        .then(function(res) {
-          chefToken = res.body.token;
-        });
-
-      var userSignup = request(app)
-        .post('/api/users/signup')
-        .send({
-          username: 'oicki',
-          password: 'hunter2',
-          zipcode: '00000'
-        })
-        .then(function(res) {
-          userToken = res.body.token;
-        });
-
-      Promise.all([chefSignup, userSignup]).then(function() {
-        done();
-      });
-    });
-
-    afterEach(function(done) {
-      db.Meal.destroy({
-        where: { name: 'rubber' }
-      }).then(function() {
-        done();
-      });
-    });
-
-    after(function(done) {
-      var chefRemove = db.Chef.destroy({
-        where: { username: 'oicki' }
-      });
-      var userRemove = db.User.destroy({
-        where: { username: 'oicki' }
-      });
-
-      Promise.all([chefRemove, userRemove]).then(function() {
-        done();
-      });
-    });
-
     it('should not allow access if no auth token is sent', function(done) {
       request(app)
         .post('/api/chefs/meals')
