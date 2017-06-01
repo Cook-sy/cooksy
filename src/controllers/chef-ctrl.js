4bdb5d5d362beab5e28d8ab60fd7952b819e766d
vicki alden <vic.alden23@gmail.com>
2017-05-31 20:34:41 -0700

    (refactor) Refactor database queries for login and signup into controller directory

--- src/controllers/chef-ctrl.js
+++ src/controllers/chef-ctrl.js
@@ -1,5 +1,17 @@
 var db = require('../models');
 
+exports.createChef = function(body, username, password) {
+  return db.Chef.create({
+    username: username.trim(),
+    password: password.trim(),
+    image: body.image.trim(),
+    address: body.address.trim(),
+    city: body.city.trim(),
+    state: body.state.trim(),
+    zipcode: body.zipcode.trim()
+  });
+};
+
 exports.findUser = function(userId) {
   return db.Chef.findById(userId);
 };
--- src/controllers/user-ctrl.js
+++ src/controllers/user-ctrl.js
@@ -1,5 +1,21 @@
 var db = require('../models');
 
+exports.createUser = function(body, username, password) {
+  return db.User.create({
+    username: username.trim(),
+    password: password.trim(),
+    zipcode: body.zipcode.trim()
+  });
+};
+
 exports.findUser = function(userId) {
   return db.User.findById(userId);
+};
+
+exports.findUserByUsername = function(username) {
+  return db.User.findOne({
+    where: {
+      username: username
+    }
+  });
 };
