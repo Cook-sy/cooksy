09b976f0a208ae865901a9918444d2d77f3ebb54
John Cheng <chengj@gmail.com>
2017-06-01 00:55:03 -0700

    (test) Add tests for login validation

--- test/unit_test/form-validation.test.js
+++ test/unit_test/form-validation.test.js
@@ -14,5 +14,29 @@ describe('Form validation', function() {
     it('should not validate if payload is not defined', sinon.test(function() {
       expect(form.validateLogin().success).to.be.false;
     }));
+
+    it('should not validate if username is empty', sinon.test(function() {
+      var validationResult = form.validateLogin({
+        username: '',
+        password: 'hunter2'
+      });
+      expect(validationResult.success).to.be.false;
+    }));
+
+    it('should not validate if password is empty', sinon.test(function() {
+      var validationResult = form.validateLogin({
+        username: 'wicki',
+        password: ''
+      });
+      expect(validationResult.success).to.be.false;
+    }));
+
+    it('should validate if username and password are non-empty', sinon.test(function() {
+      var validationResult = form.validateLogin({
+        username: 'wicki',
+        password: 'hunter2'
+      });
+      expect(validationResult.success).to.be.true;
+    }));
   });
 });
