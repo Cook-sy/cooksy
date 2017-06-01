869355a91f041482c0a6fd7b7ab41f1d8c3126eb
John Cheng <chengj@gmail.com>
2017-05-31 23:26:47 -0700

    (chore) Comment out example tests

--- test/integration_test/integration_test_example.js
+++ test/integration_test/integration_test_example.js
@@ -4,11 +4,11 @@ var expect = chai.expect;
 
 var app = require('../../src/server-config');
 
-xdescribe('/api/users', function() {
-  it('should return 200 status code', function(done) {
-    request(app).get('/api/users').end(function(err, res) {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-});
+// describe('/api/users', function() {
+//   it('should return 200 status code', function(done) {
+//     request(app).get('/api/users').end(function(err, res) {
+//       expect(res.statusCode).to.equal(200);
+//       done();
+//     });
+//   });
+// });
