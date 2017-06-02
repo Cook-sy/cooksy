c0d74d259f2ed119635317c95d803fdfb8235b62
obay <obaymardini@gmail.com>
2017-06-02 02:23:41 +0200

    (feat) Listen to the create meal action creator in the reducer

--- client/src/reducers/mealsReducer.js
+++ client/src/reducers/mealsReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_MEALS, FETCH_MEALDETAIL } from '../actions/index';
+import { FETCH_MEALS, FETCH_MEALDETAIL, CREATE_MEAL } from '../actions/index';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
@@ -7,6 +7,8 @@ export default function(state = {}, action) {
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_MEALS:
       return _.mapKeys(action.payload.data, 'id');
+    case CREATE_MEAL:
+      return { ...state, [action.payload.data.meal.id]: action.payload.data.meal };
     default:
       return state;
   }
