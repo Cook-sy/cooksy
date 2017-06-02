d25de34ecef4ffc74387cc10ff2854f4b8c6d3e4
obay <obaymardini@gmail.com>
2017-06-02 20:55:15 +0200

    (fix) Transform PrivateRoute into a componenet

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import { decodeToken } from './IsAuthenticated';
-export default class PrivateRoute extends Component {
-  render() {
-    const { component: Component, role, ...rest } = this.props;
-    const auth = decodeToken();
-    return (
-      <Route
-        {...rest}
-        render={props =>
-          auth && auth.role === role
-            ? <Component {...props} />
-            : <Redirect
-                to={{
-                  pathname: '/sign-up-form',
-                  state: { from: props.location }
-                }}
-              />}
-      />
-    );
-  }
+export default function PrivateRoute(props) {
+  const { component: Component, user, ...rest } = props;
+  const auth = decodeToken();
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        auth && auth.role === user
+          ? <Component {...props} />
+          : <Redirect
+              to={{
+                pathname: '/sign-up-form',
+                state: { from: props.location }
+              }}
+            />}
+    />
+  );
 }
