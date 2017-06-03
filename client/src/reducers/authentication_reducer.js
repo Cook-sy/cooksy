abd2c4185ee0e2d86bcf2b1b6dfa1b02aa398e92
obay <obaymardini@gmail.com>
2017-06-03 04:42:26 +0200

    (feat) Add error prop to the store

--- client/src/reducers/authentication_reducer.js
+++ client/src/reducers/authentication_reducer.js
@@ -1,42 +1,21 @@
-import { SIGN_UP_USER, SIGN_UP_CHEF, LOG_IN_USER, LOG_IN_CHEF } from '../actions';
+import { AUTHENTICATE } from '../actions';
 
 let initialState = {
-  auth: {
-    isLoggedIn: false,
-    role: false
-  }
+  error: ''
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case SIGN_UP_USER:
-      return {
-        auth: {
-          isLoggedIn: true,
-          role: 'user'
-        }
-      };
-    case SIGN_UP_CHEF:
-      return {
-        auth: {
-          isLoggedIn: true,
-          role: 'chef'
-        }
-      };
-    case LOG_IN_USER:
-      return {
-        auth: {
-          isLoggedIn: true,
-          role: 'user'
-        }
-      };
-    case LOG_IN_CHEF:
-      return {
-        auth: {
-          isLoggedIn: true,
-          role: 'chef'
-        }
-      };
+    case AUTHENTICATE:
+      if (action.error) {
+        return {
+          error: action.payload.response.data.message
+        };
+      } else {
+        return {
+          error: ''
+        };
+      }
     default:
       return state;
   }
