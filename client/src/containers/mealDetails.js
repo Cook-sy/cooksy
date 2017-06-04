62d4fb6aed9990267283ce359baa7e750419ff8e
obay <obaymardini@gmail.com>
2017-06-04 06:34:59 +0200

    (feat) Import the action creator the necessary fomr components

--- client/src/containers/mealDetails.js
+++ client/src/containers/mealDetails.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchMealDetail } from '../actions/index';
+import { fetchMealDetail, reviewMeal } from '../actions';
+import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Rating } from 'material-ui-rating';
+
+import {
+  renderTextAreaField,
+  renderTimeField,
+  renderTextField,
+  renderDateField,
+  isZipcode
+} from '../utils/FormHelper';
+import './MealDetails.css';
 
 class MealDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  submitForm(values) {
+    values.mealId = this.props.meal.id;
+    this.props.reviewMeal(values);
+  }
+
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchMealDetail(id);
   }
 
   render() {
-    const { meal } = this.props;
+    const { meal, handleSubmit, pristine, submitting } = this.props;
     if (!meal) {
       return <div>Loading...</div>;
     }
