5dbbc6042d86de5af8b93e94c79023361173f1eb
obay <obaymardini@gmail.com>
2017-06-06 06:12:28 +0200

    (feat) Add action creator to fetch meals by chef name

--- client/src/actions/mealsActions.js
+++ client/src/actions/mealsActions.js
@@ -5,6 +5,7 @@ import { attachTokenToTheHeader } from '../utils/RequestHelper';
 export const CREATE_MEAL = 'CREATE_MEAL';
 export const FETCH_MEALS = 'FETCH_MEALS';
 export const FETCH_MEALDETAIL = 'FETCH_MEALDETAIL';
+export const FETCH_MEALS_BY_CHEF = 'FETCH_MEALS_BY_CHEF';
 
 export function createMeal(values, cb) {
   const headers = attachTokenToTheHeader();
@@ -21,6 +22,15 @@ export function fetchMeals() {
 
   return {
     type: FETCH_MEALS,
+    payload: request
+  };
+}
+
+export function fetchMealsByChef() {
+  const request = axios.get('/api/meals');
+
+  return {
+    type: FETCH_MEALS_BY_CHEF,
     payload: request
   };
 }
