f53b0e3af5b866c33a94d14dfe82009dbaf0251d
obay <obaymardini@gmail.com>
2017-06-06 02:56:43 +0200

    (feat) Add a prop to the state to check whether the user has reviewed the meal or not

--- client/src/reducers/reviewReducer.js
+++ client/src/reducers/reviewReducer.js
@@ -1,11 +1,16 @@
-import { TOGGLE_REVIEW } from '../actions/index';
+import { TOGGLE_REVIEW, DID_REVIEW, DID_NOT_REVIEW } from '../actions/index';
 const initialState = {
-	addReview: false
+	addReview: false,
+	didReview: true
 }
 export default function(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_REVIEW:
       return { ...state, addReview: !state.addReview };
+    case DID_REVIEW:
+    	return {  ...state, didReview: true };
+    case DID_NOT_REVIEW:
+    	return { ...state, didReview: false };
     default:
       return state;
   }
