b00877098a2356059ccac307a16ac8045bbbfb28
obay <obaymardini@gmail.com>
2017-06-06 21:34:34 +0200

    (fix) Prohibit the chef and unregistred user from reviewing meals

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { change } from 'redux-form';
 
 import { attachTokenToTheHeader } from '../utils/RequestHelper';
-import { decodeToken as username } from '../utils/IsAuthenticated';
+import { decodeToken as userDetails } from '../utils/IsAuthenticated';
 
 export const CREATE_MEAL = 'CREATE_MEAL';
 export const FETCH_MEALS = 'FETCH_MEALS';
@@ -13,6 +13,7 @@ export const RATE_MEAL = 'RATE_MEAL';
 export const TOGGLE_REVIEW = 'TOGGLE_REVIEW';
 export const DID_REVIEW = 'DID_REVIEW';
 export const DID_NOT_REVIEW = 'DID_NOT_REVIEW';
+export const CAN_NOT_REVIEW = 'CAN_NOT_REVIEW';
 
 export function createMeal(values, cb) {
   const headers = attachTokenToTheHeader();
@@ -121,7 +122,15 @@ export function successfullReview(meal) {
 }
 
 export function didReview(meal) {
-  const user = username() && username().user;
+  const user = userDetails();
+  const username = user && user.user;
+  const role = user && user.role;
+  if (role === 'chef' || !username) {
+    return {
+      type: CAN_NOT_REVIEW
+    };
+  }
+
   const mealReviewwedByTheUser = meal.mealReviews.find(
     review => review.user.username === user
   );
