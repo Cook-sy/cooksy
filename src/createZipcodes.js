da3fb6051cf106624c87054d6aabc88d4fd1f982
John Cheng <chengj@gmail.com>
2017-06-05 18:52:25 -0700

    (feat) Add create zipcode table script

--- src/createZipcodes.js
+++ src/createZipcodes.js
@@ -5,18 +5,14 @@ var db = require('./models');
 
 var csvZipcode = [];
 
-var createZipcodes = function() {
-  return fs.createReadStream(path.join(__dirname, 'zipcodes.txt'))
-      .pipe(parse({ delimiter: ',', columns: true }))
-      .on('data', function(row) {
-        csvZipcode.push(row);
-      })
-      .on('end', function() {
-        db.Zipcode.sync({ force: true })
-          .then(function() {
-            return db.Zipcode.bulkCreate(csvZipcode);
-          });
+fs.createReadStream(path.join(__dirname, 'zipcodes.txt'))
+  .pipe(parse({ delimiter: ',', columns: true }))
+  .on('data', function(row) {
+    csvZipcode.push(row);
+  })
+  .on('end', function() {
+    db.Zipcode.sync({ force: true })
+      .then(function() {
+        return db.Zipcode.bulkCreate(csvZipcode);
       });
-};
-
-module.exports = createZipcodes;
+  });
