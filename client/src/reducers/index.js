5f206eacb4a0ecbcc16972adea8e7da58db389a8
vicki alden <vic.alden23@gmail.com>
2017-06-06 10:32:21 -0700

    (feat) Add todays meals reducer file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,12 +4,14 @@ import { reducer as ReduxFormReducer } from 'redux-form';
 import MealReducer from './mealsReducer';
 import AuthenticationReducer from './authentication_reducer';
 import ReviewReducer from './reviewReducer';
+import TodaysMealReducer from './todaysMealsReducer';
 
 const rootReducer = combineReducers({
   meals: MealReducer,
   form: ReduxFormReducer,
   auth: AuthenticationReducer,
-  review: ReviewReducer
+  review: ReviewReducer,
+  todaysMeals: TodaysMealReducer
 });
 
 export default rootReducer;
