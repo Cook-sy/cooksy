b1a402cca301c7b53b09473fb5e14d64eb467d2e
vicki alden <vic.alden23@gmail.com>
2017-06-07 14:44:30 -0700

    (feat) Remove react-responsive carousel

--- client/src/containers/Homepage.js
+++ client/src/containers/Homepage.js
@@ -6,6 +6,7 @@ import { GridList, GridTile } from 'material-ui/GridList';
 import { Rating } from 'material-ui-rating';
 import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import Carousel from 'react-responsive-carousel/lib/components/Carousel';
 import _ from 'lodash';
 
 import './Homepage.css';
@@ -19,6 +20,21 @@ class Homepage extends Component {
   render() {
     return (
       <div className="root-homepage">
+         <Carousel axis="horizontal" showThumbs={true} showArrows={true}>
+            <div>
+                <img src="https://www.organicfacts.net/wp-content/uploads/2013/05/Carrot1.jpg" />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src="https://www.organicfacts.net/wp-content/uploads/2013/05/Carrot1.jpg" />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src="https://www.organicfacts.net/wp-content/uploads/2013/05/Carrot1.jpg" />
+                <p className="legend">Legend 3</p>
+            </div>
+        </Carousel>
+
         <h2>{new Date().toDateString()}</h2>
         <GridList
           cellHeight={200}
