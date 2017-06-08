27c81375d9ca037af8a9bc04f736b3828fdb4eef
obay <obaymardini@gmail.com>
2017-06-08 04:20:21 +0200

    (fix) Change the file name of authentication reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { reducer as ReduxFormReducer } from 'redux-form';
 
 import MealReducer from './mealsReducer';
-import AuthenticationReducer from './authentication_reducer';
+import AuthenticationReducer from './authenticationReducer';
 import ReviewReducer from './reviewReducer';
 import TodaysMealReducer from './todaysMealsReducer';
 
