7ae3cd52fe000ca4e7d4ae63c980332db0f2a752
obay <obaymardini@gmail.com>
2017-06-08 04:02:35 +0200

    (feat) Connect the component to the store

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { getUserDetails } from '../actions';
 import './NavBar.css'
 
-const NavBar = () => (
-	<div>
-		<ul className="nav">
-	    <li className="active"><Link to="/">Home</Link></li>
-	    <li><Link to="/meals">Meals</Link></li>
-			<li><Link to="/post-new-meal">New Meal</Link></li>
-			<li className="nav-auth"><Link to="/sign-up-form">Signup</Link></li>
-			<li className="nav-auth"><Link to="/log-in-form">Login</Link></li>
-		</ul>
-	</div>
-);
+class NavBar extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-export default NavBar;
+	componentWillMount() {
+		this.props.getUserDetails()
+	}
+
+	render() {
+		const { auth: { user } } = this.props;
+		return (
+			<div>
+				<ul className="nav">
+			    <li className="active"><Link to="/">Home</Link></li>
+			    <li><Link to="/meals">Meals</Link></li>
+					<li><Link to="/post-new-meal">New Meal</Link></li>
+					<li className={`nav-auth ${!user.user ? null : 'hidden'}`}><Link to="/sign-up-form">Signup</Link></li>
+					<li className={`nav-auth ${!user.user ? null : 'hidden'}`}><Link to="/log-in-form">Login</Link></li>
+					<li className={`nav-auth ${user.user ? null : 'hidden' }`}><Link to="/logout">Logout</Link></li>
+				</ul>
+			</div>
+		);
+	}
+} 
+
+function mapStateToProps({ auth }) {
+	return {
+		auth: auth
+	}
+}
+export default connect(mapStateToProps, { getUserDetails })(NavBar);
