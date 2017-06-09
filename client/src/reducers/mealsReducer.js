dc9cd2f4f6d263cc8df9f8835cba9424138a47f3
obay <obaymardini@gmail.com>
2017-06-09 21:00:11 +0200

    (refactor) Move Fetch meal details and meal review to review reducer

--- client/src/reducers/mealsReducer.js
+++ client/src/reducers/mealsReducer.js
@@ -1,10 +1,14 @@
-import { FETCH_MEALS, FETCH_MEALDETAIL, CREATE_MEAL, REVIEW_MEAL, FETCH_MEALS_BY_CHEF, GET_NEAR_BY_MEALS } from '../actions/index';
+import { 
+  FETCH_MEALS,
+  CREATE_MEAL,
+  FETCH_MEALS_BY_CHEF,
+  GET_NEAR_BY_MEALS
+} from '../actions/index';
+
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
-    case FETCH_MEALDETAIL:
-      return { ...state, [action.payload.data.meal.id]: action.payload.data.meal };
     case FETCH_MEALS:
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_MEALS_BY_CHEF:
@@ -13,11 +17,6 @@ export default function(state = {}, action) {
       return _.mapKeys(action.payload.data, 'id'); 
     case CREATE_MEAL:
       return { ...state, [action.payload.data.meal.id]: action.payload.data.meal };
-    case REVIEW_MEAL:
-      const id = Object.keys(state)[0];
-      const newState = { ...state };
-      newState[id].mealReviews = [...state[id].mealReviews, action.payload.data.review];
-      return newState;
     default:
       return state;
   }
