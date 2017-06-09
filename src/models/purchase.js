f57a4463ced356254d4a5cb3e7b0359ec777e6f1
John Cheng <chengj@gmail.com>
2017-06-09 11:25:40 -0700

    (feat) Add purchase hooks to update meal num ordered

--- src/models/purchase.js
+++ src/models/purchase.js
@@ -28,5 +28,32 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
+
+  var updateNumOrdered = function(mealId) {
+    return sequelize.models.Meal.findById(mealId)
+      .then(function(meal) {
+        return meal.updateNumOrdered();
+      });
+  };
+
+  Purchase.beforeCreate(function(purchase, options) {
+    return sequelize.models.Meal.findById(purchase.mealId)
+      .then(function(meal) {
+        purchase.individualPrice = meal.price;
+      });
+  });
+
+  Purchase.afterCreate(function(purchase, options) {
+    return updateNumOrdered(purchase.mealId);
+  });
+
+  Purchase.afterDestroy(function(purchase, options) {
+    return updateNumOrdered(purchase.mealId);
+  });
+
+  Purchase.afterUpdate(function(purchase, options) {
+    return updateNumOrdered(purchase.mealId);
+  });
+
   return Purchase;
 };
