48b019309ae37799f61ea75c43b97824ab5e60c7
obay <obaymardini@gmail.com>
2017-06-10 00:57:11 +0200

    (feat) Add request actions

---
+++ client/src/actions/requestActions.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { attachTokenToTheHeader } from '../utils/RequestHelper';
+
+export const CREATE_REQUEST = 'CREATE_REQUEST';
+
+export function createRequest(values) {
+	console.log('RequestHelper')
+  const headers = attachTokenToTheHeader();
+  const request = axios.post('/api/chefs/requests', values, { headers: headers });
+
+  return {
+    type: CREATE_REQUEST,
+    payload: request
+  };
+}
