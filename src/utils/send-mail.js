c7900af168c4eaafff471f48e0b14bcbdaa37a74
John Cheng <chengj@gmail.com>
2017-06-10 16:09:25 -0700

    (feat) Change request mail sender to use templates

--- src/utils/send-mail.js
+++ src/utils/send-mail.js
@@ -1,5 +1,12 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
 var nodemailer = require('nodemailer');
 var htmlToText = require('nodemailer-html-to-text').htmlToText;
+
+var templatePath = function(templateName) {
+  return path.join(__dirname, '../views/emails/' + templateName + '.html');
+};
 
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -55,19 +62,40 @@ mail.sendPurchase = function(purchase) {
 
 mail.sendRequest = function(req) {
   var subject = req.user.username + ' just requested ' + req.num + ' of ' + req.request.meal.name;
-  var message = req.user.username + ' just requested ' + req.num + ' of ' + req.request.meal.name + '.\n';
 
-  var mailOptions = {
-    from: process.env.MAIL_USER,
-    bcc: [req.user.email, req.request.meal.chef.email],
-    subject: subject,
-    html: message
+  var templateContentUser = fs.readFileSync(templatePath('requests-user'), { encoding: 'utf8' });
+  var templateContentChef = fs.readFileSync(templatePath('requests-chef'), { encoding: 'utf8' });
+  var compiledUser = _.template(templateContentUser);
+  var compiledChef = _.template(templateContentChef);
+
+  var data = {
+    username: req.user.username,
+    chefUsername: req.request.meal.chef.username,
+    dateRequested: req.createdAt,
+    mealName: req.request.meal.name,
+    quantity: req.num,
+    deadline: req.request.deadline
   };
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      return console.log(error);
-    }
+  var mailOptionsUser = {
+    from: process.env.MAIL_USER,
+    to: req.user.email,
+    subject: subject,
+    html: compiledUser(data)
+  };
+  var mailOptionsChef = {
+    from: process.env.MAIL_USER,
+    to: req.request.meal.chef.email,
+    subject: subject,
+    html: compiledChef(data)
+  };
+
+  transporter.sendMail(mailOptionsUser, function(error, info) {
+    if (error) { return console.log(error); }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  transporter.sendMail(mailOptionsChef, function(error, info) {
+    if (error) { return console.log(error); }
     console.log('Message %s sent: %s', info.messageId, info.response);
   });
 };
