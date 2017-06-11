b6e67b61285d4243d70e891d4b716b50c6bf3692
vicki alden <vic.alden23@gmail.com>
2017-06-11 10:25:32 -0700

    (fix) Change user profile route to be private

--- client/src/index.js
+++ client/src/index.js
@@ -39,7 +39,7 @@ ReactDOM.render(
             <Route exact path="/signup" component={SignUpForm} />
             <Route exact path="/login" component={LogInForm} />
             <Route exact path="/chef-profile" component={ChefProfile}/>
-            <Route exact path="/user-profile" component={UserProfile}/>
+            <PrivateRoute exact user='user'path="/user-profile" component={UserProfile}/>
             <PrivateRoute exact user='chef' path="/post-new-meal" component={NewMealForm} />
             <PrivateRoute exact user='chef' path="/request-form" component={RequestForm}/>
           </Switch>
