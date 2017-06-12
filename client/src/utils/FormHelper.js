b3c0b63e3a472d16554e40e6957180587d2c3954
obay <obaymardini@gmail.com>
2017-06-12 17:50:36 +0200

    (feat) Validate email address

--- client/src/utils/FormHelper.js
+++ client/src/utils/FormHelper.js
@@ -16,6 +16,11 @@ export function isZipcode(value) {
   return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(value);
 }
 
+export function isEmail(email) {
+  var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+}
+
 export const renderTextField = ({
   input,
   label,
