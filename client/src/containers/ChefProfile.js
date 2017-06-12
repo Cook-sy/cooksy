79bb1b590c4deab2fda36b707ef71a3ba6b4e9f2
Robert Si <robertsi0214@gmail.com>
2017-06-12 14:28:29 -0700

    (feat) updated chefs profile

--- client/src/containers/ChefProfile.js
+++ client/src/containers/ChefProfile.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Avatar from 'material-ui/Avatar';
@@ -5,12 +6,16 @@ import List from 'material-ui/List/List';
 import ListItem from 'material-ui/List/ListItem';
 import { Rating } from 'material-ui-rating';
 import { fetchMealsByChef } from '../actions/index';
+import HorizontalGrid from '../components/HorizontalGrid';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import './ChefProfile.css';
+
 
 class ChefProfile extends Component {
   componentDidMount() {
     this.props.fetchMealsByChef();
+    console.log(this.props.meals);
   }
 
   render() {
@@ -21,23 +26,31 @@ class ChefProfile extends Component {
     }
 
     return (
+      <div>
       <List>
         <ListItem
           disabled={true}
           leftAvatar={<Avatar src={chef.image} size={300} />}
         />
         <div className="chef-info">
-          <h3>{chef.name}</h3>
+          <h3>{chef.username}</h3>
           <Rating value={Math.ceil(chef.rating)} max={5} readOnly={true} />
+          <RaisedButton className="request" label="Request a Meal" primary={true} />
         </div>
       </List>
+      {meals.map((meal) => (
+        <div>
+          <HorizontalGrid key={meal.id} meals={this.props.fetchMealsByChef()}/>
+        </div>
+      ))}
+      </div>
     );
   }
 }
 
 function mapStateToProps({ meals }) {
   return {
-    meals,
+    meals: meals,
     chef: meals[0] && meals[0].chef
   };
 }
