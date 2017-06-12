f9baa10fba132817e0f53ce458afe72a2a06e15b
vicki alden <vic.alden23@gmail.com>
2017-06-12 11:44:00 -0700

    (cleanup) Fix indentation

--- client/src/containers/UserProfile.js
+++ client/src/containers/UserProfile.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { GridList, GridTile } from 'material-ui/GridList';
+import {
+  GridList,
+  GridTile
+} from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
 import { Rating } from 'material-ui-rating';
-import {Card, CardTitle, CardText} from 'material-ui/Card';
+import {
+  Card,
+  CardTitle,
+  CardText
+} from 'material-ui/Card';
 import _ from 'lodash';
 
 import { getPurchases } from '../actions/purchaseActions';
@@ -28,7 +35,7 @@ class UserProfile extends Component {
               <CardTitle
                 actAsExpander={true}
                 showExpandableButton={true}
-                title={<p>{purchase.meal.name}, {new Date(purchase.createdAt).toString().substr(4, 11)}</p>}
+                title={<p className="meal-title">{purchase.meal.name}, {new Date(purchase.createdAt).toString().substr(4, 11)}</p>}
                 subtitle={<span>by <b>{purchase.meal.chef.username}</b></span>}
               >
                 <p>${purchase.individualPrice}<br/>
@@ -38,8 +45,14 @@ class UserProfile extends Component {
                 <img className="purchase-images" src={purchase.meal.images} alt={purchase.meal.name} />
               </CardTitle>
               <CardText expandable={true}>
-                <p><strong>Pick up info:</strong><br/>{new Date(purchase.meal.deliveryDateTime).toString().substr(4, 11)}<br/>{new Date(purchase.meal.deliveryDateTime).toLocaleTimeString()}<br/>{purchase.meal.pickupInfo}</p>
-                <p>{purchase.meal.address}<br/>{purchase.meal.city}, {purchase.meal.state} {purchase.meal.zipcode}</p>
+                <p><strong>Pick up info:</strong><br/>
+                  {new Date(purchase.meal.deliveryDateTime).toString().substr(4, 11)}<br/>
+                  {new Date(purchase.meal.deliveryDateTime).toLocaleTimeString()}<br/>
+                  {purchase.meal.pickupInfo}
+                </p>
+                <p>{purchase.meal.address}<br/>
+                  {purchase.meal.city}, {purchase.meal.state} {purchase.meal.zipcode}
+                </p>
                 <CardTitle
                   title={<Link to={`/meals/${purchase.meal.id}`} style={{color:'blue', textDecoration: 'none'}}>Veiw Meal Details</Link>}/>
               </CardText>
