1794036405d28aa8e7344d78ae7fe0f264d3a602
Vicki Alden <vic.alden23@gmail.com>
2017-06-13 15:15:38 -0700

    Create purchaseActions.js

--- client/src/actions/purchaseActions.js
+++ client/src/actions/purchaseActions.js
@@ -6,8 +6,6 @@ import { decodeToken as userDetails } from '../utils/IsAuthenticated';
 export const GET_PURCHASES = 'GET_PURCHASES';
 
 export function getPurchases() {
-  const user = userDetails();
-  const userId = user && user.sub;
   const headers = attachTokenToTheHeader();
   const request = axios.get('/api/users/purchases', { headers });
 
