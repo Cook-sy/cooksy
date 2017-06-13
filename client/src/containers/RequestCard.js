25f7e8a265497ab3eff2c9c3b1d0792afa21748f
obay <obaymardini@gmail.com>
2017-06-14 00:21:10 +0200

    (feat) Add a callback to request a meal

--- client/src/containers/RequestCard.js
+++ client/src/containers/RequestCard.js
@@ -7,21 +7,26 @@ import { Link } from 'react-router-dom';
 import { differenceBetweenTwoDatesInDays } from '../utils/FormHelper';
 import './RequestCard.css';
 
-export default function RequestCard(props)  {
-	const ordersLeft = (props.numOrdered / props.numRequired) * 100;
-	const countdown =  differenceBetweenTwoDatesInDays(props.deadline)
+export default function RequestCard(props) {
+  const ordersLeft = props.numOrdered / props.numRequired * 100;
+  const countdown = differenceBetweenTwoDatesInDays(props.deadline);
   return (
     <div className="request-card">
- 	    <LinearProgress mode="determinate" value={ordersLeft} />
-        <div className="request-stat">
-	     	<span>{props.numOrdered} <i className="material-icons cyan">favorite</i></span>
-	     	<span className="num-required">{props.numRequired} <i className="material-icons cyan">restaurant</i></span>
+      <LinearProgress mode="determinate" value={ordersLeft} />
+      <div className="request-stat">
+        <span>
+          {props.numOrdered} <i className="material-icons cyan">favorite</i>
+        </span>
+        <span className="num-required">
+          {props.numRequired} <i className="material-icons cyan">restaurant</i>
+        </span>
       </div>
       <p>Countdown: {countdown} Days</p>
       <div className="request-buttons">
-        <RaisedButton 
-          onClick={props.requestMeal} 
-          label={'Request'} 
+        <RaisedButton
+          onClick={() =>
+            props.orderRequestedMeal({ num: 1, requestId: props.requestId })}
+          label={'Request'}
           backgroundColor="rgb(0, 188, 212)"
           icon={<i className="material-icons white">favorite</i>}
         />
@@ -30,7 +35,7 @@ export default function RequestCard(props)  {
           label={'Meal Details'}
           backgroundColor="rgb(0, 188, 212)"
         />
-	    </div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 }
