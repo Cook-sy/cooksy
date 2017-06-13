62c8051aca0acfb7954036dbc8e21ba7c28fabb8
obay <obaymardini@gmail.com>
2017-06-13 16:54:47 +0200

    (feat) Add progress bar and request button to request card component

--- client/src/containers/RequestCard.js
+++ client/src/containers/RequestCard.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import connect from 'react-redux';
 import LinearProgress from 'material-ui/LinearProgress';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class RequestCard extends Component {
-  render() {
-  	return (
-  		<div>
-	  		<LinearProgress mode="indeterminate" />
-  		</div>	
-  	);
-  }
+import { Link } from 'react-router-dom';
+import { differenceBetweenTwoDatesInDays } from '../utils/requestHelper';
+import './RequestCard.css';
+
+export default function RequestCard(props)  {
+	const ordersLeft = (props.numOrdered / props.numRequired) * 100;
+	const countdown =  differenceBetweenTwoDatesInDays(props.deadline)
+  return (
+    <div className="request-card">
+ 	    <LinearProgress mode="determinate" value={ordersLeft} />
+        <div className="request-stat">
+	     	<span>{props.numOrdered} <i className="material-icons cyan">favorite</i></span>
+	     	<span className="num-required">{props.numRequired} <i className="material-icons cyan">restaurant</i></span>
+      </div>
+      <p>Countdown: {countdown} Days</p>
+      <div className="request-buttons">
+        <RaisedButton 
+          onClick={props.requestMeal} 
+          label={'Request'} 
+          backgroundColor="rgb(0, 188, 212)"
+          icon={<i className="material-icons white">favorite</i>}
+        />
+        <RaisedButton
+          containerElement={<Link to={`/meals/${props.mealId}`} />}
+          label={'Meal Details'}
+          backgroundColor="rgb(0, 188, 212)"
+        />
+	    </div>
+		</div>
+	);
 }
-
-export default RequestCard;
