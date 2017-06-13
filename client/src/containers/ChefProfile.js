4c94c516ae127e1ba3865eecfd80e6fb69b4b7f3
Robert Si <robertsi0214@gmail.com>
2017-06-13 09:36:30 -0700

    (feat) added horizontal bar to chef profile

--- client/src/containers/ChefProfile.js
+++ client/src/containers/ChefProfile.js
@@ -15,34 +15,29 @@ import './ChefProfile.css';
 class ChefProfile extends Component {
   componentDidMount() {
     this.props.fetchMealsByChef();
-    console.log(this.props.meals);
   }
 
   render() {
     const { meals, chef } = this.props;
-
     if (!meals || !chef) {
       return <p>Loading.....</p>;
     }
-
     return (
       <div>
-      <List>
-        <ListItem
-          disabled={true}
-          leftAvatar={<Avatar src={chef.image} size={300} />}
-        />
-        <div className="chef-info">
-          <h3>{chef.username}</h3>
-          <Rating value={Math.ceil(chef.rating)} max={5} readOnly={true} />
-          <RaisedButton className="request" label="Request a Meal" primary={true} />
-        </div>
-      </List>
-      {meals.map((meal) => (
-        <div>
-          <HorizontalGrid key={meal.id} meals={this.props.fetchMealsByChef()}/>
-        </div>
-      ))}
+        <List>
+          <ListItem
+            disabled={true}
+            leftAvatar={<Avatar src={chef.image} size={300} />}
+          />
+          <div className="chef-info">
+            <h3>{chef.username}</h3>
+            <Rating value={Math.ceil(chef.rating)} max={5} readOnly={true} />
+            <RaisedButton className="request" label="Request a Meal" primary={true} />
+          </div>
+        </List>
+          <div className="griddiv">
+            <HorizontalGrid meals={this.props.meals}/>
+          </div>
       </div>
     );
   }
