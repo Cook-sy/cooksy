f5360dbea1ee051bac1408fc58d5a08f50028bb1
obay <obaymardini@gmail.com>
2017-06-15 17:50:29 +0200

    (feat) Use can request meals in the homepage

--- client/src/components/RequestGridElement.js
+++ client/src/components/RequestGridElement.js
@@ -5,13 +5,13 @@ import { Rating } from 'material-ui-rating';
 
 import RequestCard from './RequestCard';
 
-const RequestGridElement = function({ gridItem }) {
+const RequestGridElement = function({ gridItem, orderRequestedMeal }) {
   return (
     <RequestCard
       requestId={gridItem.requestId}
       numRequired={gridItem.request.numRequired}
       numOrdered={gridItem.request.numOrdered}
-      
+      orderRequestedMeal={orderRequestedMeal}
       deadline={gridItem.request.deadline}
       meal={gridItem.request.meal}
     />
