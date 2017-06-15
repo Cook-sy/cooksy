367878092df99048cef087e43ffef4df423d51c4
Robert Si <robertsi0214@gmail.com>
2017-06-14 17:47:43 -0700

    (feat) add post to action and reducer

--- client/src/actions/purchaseActions.js
+++ client/src/actions/purchaseActions.js
@@ -4,6 +4,7 @@ import { attachTokenToTheHeader } from '../utils/RequestHelper';
 import { decodeToken as userDetails } from '../utils/IsAuthenticated';
 
 export const GET_PURCHASES = 'GET_PURCHASES';
+export const POST_PURCHASE = 'POST_PURCHASE';
 
 export function getPurchases() {
   const headers = attachTokenToTheHeader();
@@ -11,6 +12,15 @@ export function getPurchases() {
 
   return {
     type: GET_PURCHASES,
+    payload: request
+  };
+}
+
+export function postPurchaseDetails(values) {
+  const request = axios.post('/api/users/purchases', values, { headers: headers });
+
+  return {
+    type: POST_PURCHASE,
     payload: request
   };
 }
--- client/src/reducers/purchaseReducer.js
+++ client/src/reducers/purchaseReducer.js
@@ -1,10 +1,13 @@
 import { GET_PURCHASES } from '../actions';
+import { POST_PURCHASE } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case GET_PURCHASES:
       return _.mapKeys(action.payload.data, 'id');
+    case POST_PURCHASE:
+      return _.mapKeys(action.payload)
     default:
       return state;
   }
