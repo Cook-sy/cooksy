04da670ce8bd429256b8b6b5516e925d46760894
obay <obaymardini@gmail.com>
2017-06-15 17:33:31 +0200

    (feat) Add a helper function to get the top requests

--- client/src/utils/RequestHelper.js
+++ client/src/utils/RequestHelper.js
@@ -1,5 +1,12 @@
+import _ from 'lodash';
+
 export function attachTokenToTheHeader() {
   const token = localStorage.getItem('cooksy');
   
   return {'x-access-token': `Bearer ${token}`};
 }
+
+export function getTopRequests(requests, num = 3) {
+  return _.orderBy(requests, ['request.deadline'], ['asc'])
+  .slice(0, num);
+} 
