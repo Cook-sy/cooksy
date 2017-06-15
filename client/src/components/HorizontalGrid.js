9ff3b77d16d4e02fcb491044e90d6f4753a993ef
obay <obaymardini@gmail.com>
2017-06-15 16:48:08 +0200

    (refactor) Refactor HorizontalGrid component to be more reuseable

--- client/src/components/HorizontalGrid.js
+++ client/src/components/HorizontalGrid.js
@@ -4,23 +4,15 @@ import { Link } from 'react-router-dom';
 import { GridList, GridTile } from 'material-ui/GridList';
 import { Rating } from 'material-ui-rating';
 
-const HorizontalGrid = ({ meals }) => {
+const HorizontalGrid = ({ gridObject, GridComponent }) => {
   return (
     <GridList
       cellHeight={200}
       className="grid"
       cols={2.2}
     >
-      {_.map(meals, (meal) => (
-        <GridTile
-          className="tile"
-          key={meal.name}
-          title={<Link to={`/meals/${meal.id}`} style={{color:'white', textDecoration: 'none'}}>{meal.name}</Link>}
-          subtitle={<span>by <b>{meal.chef.username}</b></span>}
-          actionIcon={<Rating value={Math.ceil(meal.rating)} max={5} readOnly={true} />}
-        >
-          <img src={meal.images} alt={meal.name}/>
-        </GridTile>
+      {_.map(gridObject, (gridItem) => (
+        <GridComponent gridItem={gridItem} />
       ))}
     </GridList>
   );
