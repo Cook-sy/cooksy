ff9eaa2f6ee62e3c60086d17f0002e877da869ab
Robert Si <robertsi0214@gmail.com>
2017-06-15 23:38:48 -0700

    (feat) Handle post purchase action in purchase reducer

--- client/src/reducers/purchaseReducer.js
+++ client/src/reducers/purchaseReducer.js
@@ -1,10 +1,12 @@
-import { GET_PURCHASES } from '../actions';
+import { GET_PURCHASES, POST_PURCHASE } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case GET_PURCHASES:
       return _.mapKeys(action.payload.data, 'id');
+    case POST_PURCHASE:
+      return state;
     default:
       return state;
   }
