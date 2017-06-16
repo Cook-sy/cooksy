c86b455e08d88e8ab85dac0a89e1b8a8233bbd52
John Cheng <chengj@gmail.com>
2017-06-15 23:09:36 -0700

    (cleanup) Remove commented code and unused packages

--- client/src/containers/UserProfile.js
+++ client/src/containers/UserProfile.js
@@ -5,7 +5,6 @@ import {
   Card,
   CardHeader,
   CardMedia,
-  CardTitle,
   CardText
 } from 'material-ui/Card';
 import _ from 'lodash';
@@ -27,10 +26,6 @@ class UserProfile extends Component {
 
   render() {
     const { purchases, requests } = this.props;
-
-    if ( Object.keys(requests).length === 0 || Object.keys(requests).length === 0) {
-      return <div>loadding....</div>;
-    }
 
     return (
       <div className="user-root">
@@ -124,56 +119,3 @@ function mapStateToProps({ purchase, requests }) {
 }
 
 export default connect(mapStateToProps, { getPurchases, getUsersRequests, orderRequestedMeal })(UserProfile);
-
-
-// <div className="col">
-//   <h1 className="purchase-title">Purchase History</h1>
-//   {_.map(purchase, (purchase) => (
-//     <Card
-//       key={purchase.id}
-//       className="card">
-//       <CardTitle
-//         showExpandableButton={true}
-//         title={<Link to={`/meals/${purchase.meal.id}`} style={{color:'blue', textDecoration: 'none'}}>{purchase.meal.name}, {new Date(purchase.createdAt).toString().substr(4, 11)}</Link>}
-//         subtitle={<span>by <b>{purchase.meal.chef.username}</b></span>}
-//       >
-//         <p>${purchase.individualPrice}<br/>
-//           Quantity ordered: {purchase.num}<br/>
-//           Total: ${purchase.individualPrice * purchase.num}
-//         </p>
-//         <img className="purchase-images" src={purchase.meal.images} alt={purchase.meal.name} />
-//       </CardTitle>
-//       <CardText actAsExpander={true}>
-//       </CardText>
-//       <CardText expandable={true}>
-//         <p><strong>Pick up info:</strong><br/>
-//           {new Date(purchase.meal.deliveryDateTime).toString().substr(4, 11)}<br/>
-//           {new Date(purchase.meal.deliveryDateTime).toLocaleTimeString()}<br/>
-//           {purchase.meal.pickupInfo}
-//         </p>
-//         <p>{purchase.meal.address}<br/>
-//           {purchase.meal.city}, {purchase.meal.state} {purchase.meal.zipcode}
-//         </p>
-//       </CardText>
-//     </Card>
-//   ))}
-// </div>
-// <div className="col">
-//   <h1 className="purchase-title">Requests</h1>
-//   <div>
-//   <Card className="card">
-//     {_.map(requests, (request) => (
-//       <Card key={request.requestId} >
-//         <RequestCard
-//           requestId={request.requestId}
-//           numRequired={request.request.numRequired}
-//           numOrdered={request.request.numOrdered}
-//           orderRequestedMeal={this.props.orderRequestedMeal}
-//           deadline={request.request.deadline}
-//           meal={request.request.meal}
-//         />
-//       </Card>
-//     ))}
-//   </Card>
-//   </div>
-// </div>
