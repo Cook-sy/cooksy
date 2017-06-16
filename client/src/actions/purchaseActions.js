6a9f0b3bcc3c74fb1f37e6565daa740860c66479
Robert Si <robertsi0214@gmail.com>
2017-06-15 23:37:55 -0700

    (feat) Add action creator to post a purchase

--- client/src/actions/purchaseActions.js
+++ client/src/actions/purchaseActions.js
@@ -3,13 +3,24 @@ import axios from 'axios';
 import { attachTokenToTheHeader } from '../utils/RequestHelper';
 
 export const GET_PURCHASES = 'GET_PURCHASES';
+export const POST_PURCHASE = 'POST_PURCHASE';
 
 export function getPurchases() {
   const headers = attachTokenToTheHeader();
   const request = axios.get('/api/users/purchases', { headers });
 
-  return {
+ return {
     type: GET_PURCHASES,
+    payload: request
+  };
+}
+
+export function postPurchaseDetails(values) {
+  const headers = attachTokenToTheHeader();
+  const request = axios.post('/api/users/purchases', values, { headers: headers });
+
+  return {
+    type: POST_PURCHASE,
     payload: request
   };
 }
